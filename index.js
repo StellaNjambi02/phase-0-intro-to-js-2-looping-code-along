6ce3c7730818ea97da1e0ca156c3fbaf928ef06b
StellaNjambi02 <njambindungistella@gmail.com>
2024-06-28 18:02:11 +0300

    Looping

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// for (let age=30;age<40;age++){
+//     console.log(`I'm ${age} years old.`)
+// }
+
+function writeCards(arrayName,event){
+    const newArray=[];
+for(let i=0;i<arrayName.length;i++){
+    console.log(i);
+
+  newArray.push(`Thank you, ${arrayName[i]}, for the wonderful ${event} gift!`)
+}
+
+return newArray;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num){
+    for(let i=num;i>=0;i--){
+        console.log(i);
+    }
+}
